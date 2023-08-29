a1ce8c2ac1196a9f6408ad8e565817afab80548d
@NISHAN <140871999+ASHADEEP-NISHAN@users.noreply.github.com>
2023-08-29 19:09:10 +0530

    Add files via upload

--- hangman_game.py
+++ hangman_game.py
@@ -1,5 +1,20 @@
 import random as r
-word_list=['apple','mango','orange']
+word_list=[
+    "cat", "dog", "elephant", "lion", "tiger", "giraffe", "zebra", "monkey", "kangaroo", "dolphin",
+    "apple", "banana", "orange", "grape", "strawberry", "watermelon", "pear", "kiwi", "mango", "pineapple",
+    "sparrow", "eagle", "hawk", "robin", "pigeon", "parrot", "owl", "crow", "duck", "swan",
+    "rabbit", "hamster", "guinea pig", "squirrel", "ferret", "chinchilla", "rat", "mouse", "gerbil", "hedgehog",
+    "peacock", "flamingo", "penguin", "ostrich", "emu", "turkey", "chicken", "goose", "quail", "pheasant",
+    "strawberry", "blueberry", "raspberry", "blackberry", "cranberry", "cherry", "plum", "apricot", "peach", "fig",
+    "giraffe", "hippo", "rhino", "crocodile", "alligator", "snake", "lizard", "turtle", "tortoise", "gecko",
+    "wolf", "fox", "coyote", "hyena", "jackal", "lynx", "panther", "cougar", "bobcat", "cheetah",
+    "elephant", "rhinoceros", "hippopotamus", "buffalo", "gorilla", "orangutan", "chimpanzee", "bonobo", "lemur", "koala",
+    "jellyfish", "octopus", "starfish", "clam", "lobster", "shrimp", "crab", "squid", "nautilus", "coral",
+    "koala", "kangaroo", "wombat", "platypus", "wallaby", "possum", "echidna", "cassowary", "quokka", "numbat",
+    "ladybug", "butterfly", "dragonfly", "ant", "bee", "wasp", "moth", "grasshopper", "cricket", "spider"
+
+]
+
 hangman_stages = [
     """
        _________
@@ -58,7 +73,16 @@ hangman_stages = [
                 |
     """
 ]
-
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+print(logo)
 word=r.choice(word_list)
 # print(word)
 l=len(word)
@@ -90,7 +114,8 @@ while display != split and chance >0:
             print(display)
 if chance == 0:
     print("\nyou lose")
+    print("THE WORD IS:",word)
 else:
     print("\ncongratulations you guess the word")
-
+    print("THE WORD IS:", word)
 
